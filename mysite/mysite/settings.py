b085135f33701b73576a89253b253d15e47bcab0
Sahas <sahas052@gmail.com>
2012-11-09 02:25:45 +0530

    time and dynamic urls

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,10 +11,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'django',                      # Or path to database file if using sqlite3.
-        'USER': 'sahas',                      # Not used with sqlite3.
-        'PASSWORD': 'welcome',                  # Not used with sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
     }
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
-from mysite.views import hello
+from mysite.views import hello, current_datetime, hours_ahead
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',('^hello/$',hello),
+urlpatterns = patterns('',('^hello/$',hello), ('^time/$', current_datetime), 
+	('^time/plus/(\d{1,2})/$', hours_ahead)
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^mysite/', include('mysite.foo.urls')),
